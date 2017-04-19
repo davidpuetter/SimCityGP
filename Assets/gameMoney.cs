d4ec3e944c1fe03a09cf94176fde9b7e74700f29
sapoole1212 <sapoole1212@gmail.com>
2017-04-19 14:26:36 +0000

    gameMoney update

--- Assets/gameMoney.cs
+++ Assets/gameMoney.cs
@@ -1,23 +1,24 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
 
-public class gameMoney : MonoBehaviour {
+public class gameMoney : MonoBehaviour
+{
 
     public static gameMoney instance;
-    
+
     //the variables that hold the current amount of a "currency"
     public float Money = 1f;
     public float Resources = 1f;
     public float Population = 1f;
 
     //cash per second and population per second
-    public float CPS = 1f;
+    //public float CPS = 1f;
     public float PPS = 1f;
 
     //Current max population,
     public float PopulationMax = 1f;
-    
+
     //Starting cost of the buildings and alters as more are bought
     public float shopCost = 1f;
     public float houseCost = 1f;
@@ -33,9 +34,9 @@ public class gameMoney : MonoBehaviour {
     public float SelectedBuilding = 0f;
 
     //the frequency of cash collecting
-        //tracks the countdown value
+    //tracks the countdown value
     public float cashTimer = 1f;
-        //the length of the interval between cash collection
+    //the length of the interval between cash collection
     public float cashCooldown = 1f;
 
     //links the UI text to the changing values
@@ -57,8 +58,23 @@ public class gameMoney : MonoBehaviour {
     public float armoryResourcesGiven = 1f;
     public float resourceCost;
 
+    //upkeep variables taking account the cost of upkeeping buildings
+    public float tavernupkeep;
+    public float houseupkeep;
+    public float shopupkeep;
+    public float armoryupkeep;
+
+    //arrays to keep track of number of instances of each game object for example getHouse = number of houses in the game
+    private GameObject[] getHouse;
+    private GameObject[] getTavern;
+    private GameObject[] getShop;
+    private GameObject[] getArmory;
+
+    float populationmoneymod;
+
+
     //current gold multiplier from shops
-    public float shopProfits = 1f;
+    public float shopProfits = 0f;
 
     //allows this object to be called elsewhere
     void Awake()
@@ -72,10 +88,21 @@ public class gameMoney : MonoBehaviour {
 
 
 
-    public void Update ()
+    public void Update()
     {
         //countsdown the timer in real time
         cashTimer -= Time.deltaTime;
+
+        //finds how many instances of a object with a tag at runtime
+        getHouse = GameObject.FindGameObjectsWithTag("House");
+        getArmory = GameObject.FindGameObjectsWithTag("Armory");
+        getTavern = GameObject.FindGameObjectsWithTag("Tavern");
+        getShop = GameObject.FindGameObjectsWithTag("Shop");
+
+        //calculates the total upkeep of each type of building 
+        houseupkeep = getHouse.Length / 10;
+
+
 
         //when the countdown hits 0
         if (cashTimer <= 0)
@@ -84,7 +111,9 @@ public class gameMoney : MonoBehaviour {
             cashTimer = cashCooldown;
 
             //---------MAIN MONEY EQUATION---------
-            Money = Money + (CPS + (shopProfits * Population));
+            Money = Money + (shopProfits + populationmoneymod);
+
+
 
             //if the population is at its max
             if (Population + PPS >= PopulationMax)
@@ -93,20 +122,20 @@ public class gameMoney : MonoBehaviour {
                 Population = PopulationMax;
             }
             else
-            //increase by the current PPS 
-            Population = Population + PPS;
-
+                //increase by the current PPS 
+                Population = Population + PPS;
+                populationmoneymod = Population / 10;
         }
 
         //fills the labels with the values that change
-        moneyText.text = "Gold: " + System.Math.Round(Money,0).ToString() + 'G';
+        moneyText.text = "Gold: " + System.Math.Round(Money, 0).ToString() + 'G';
         populationText.text = "Population: " + Mathf.Round(Population).ToString() + '/' + PopulationMax.ToString();
         powerText.text = "Resources: " + Resources.ToString();
 
-        CPMText.text = '+' + System.String.Format("{0:n}", System.Math.Round((CPS + (shopProfits * Population)), 2).ToString()) + "Gp/s";
+        CPMText.text = '+' + System.String.Format("{0:n}", System.Math.Round(((shopProfits + populationmoneymod)), 2).ToString()) + "Gp/s";
         PPSText.text = '+' + System.Math.Round(PPS, 2).ToString() + "pp/s";
 
-        housecostLbl.text = System.Math.Round(houseCost,0).ToString() + 'G';
+        housecostLbl.text = System.Math.Round(houseCost, 0).ToString() + 'G';
         shopcostLbl.text = System.Math.Round(shopCost, 0).ToString() + 'G';
         taverncostLbl.text = System.Math.Round(tavernCost, 0).ToString() + 'G';
         armorycostLbl.text = System.Math.Round(armoryCost, 0).ToString() + 'G';
