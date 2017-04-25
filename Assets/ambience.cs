f66c5a5d07d162e9168a6d62b60d06e254adb9b0
Kieran Green <kieranu@hotmail.co.uk>
2017-04-25 18:12:42 +0100

    Kieranu - Audio Update 3
    Ambience and In Game Music added, just need sounds now.

---
+++ Assets/ambience.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ambience : MonoBehaviour {
+
+    public AudioSource[] noise;
+    public AudioSource small;
+    public AudioSource medium;
+    public AudioSource large;
+
+    bool smallPlaying;
+    bool mediumPlaying;
+    bool largePlaying;
+
+    public GameObject master;
+    private gameMoney gameManager;
+
+    // Use this for initialization
+    void Start() {
+
+        smallPlaying = false;
+        mediumPlaying = false;
+        largePlaying = false;
+
+        noise = GetComponents<AudioSource>();
+        small = noise[0];
+        medium = noise[1];
+        large = noise[2];
+
+        small.loop = true;
+        medium.loop = true;
+        large.loop = true;
+
+        gameManager = master.GetComponent<gameMoney>();
+
+        playMusic();
+    }
+
+    void playMusic() {
+        
+
+        if (gameManager.Population >= 0.0f && gameManager.Population < 50.0f)
+        {
+            small.Stop();
+            medium.Stop();
+            large.Stop();
+
+            small.Play();
+
+            smallPlaying = true;
+            mediumPlaying = false;
+            largePlaying = false;
+
+        }
+
+        if (gameManager.Population >= 50.0f && gameManager.Population < 100.0f)
+        {
+            small.Stop();
+            medium.Stop();
+            large.Stop();
+
+            medium.Play();
+
+            smallPlaying = false;
+            mediumPlaying = true;
+            largePlaying = false;
+        }
+
+        if (gameManager.Population >= 100.0f)
+        {
+            small.Stop();
+            medium.Stop();
+            large.Stop();
+
+            large.Play();
+
+            smallPlaying = false;
+            mediumPlaying = false;
+            largePlaying = true;
+        }
+    }
+
+
+    // Update is called once per frame
+    void Update() {
+
+        if (gameManager.Population >= 0.0f && gameManager.Population < 50.0f && !smallPlaying)
+        {
+            playMusic();
+        }
+
+        if (gameManager.Population >= 50.0f && gameManager.Population < 100.0f && !mediumPlaying)
+        {
+            playMusic();
+        }
+
+        if (gameManager.Population >= 100.0f && !largePlaying)
+        {
+            playMusic();
+        }
+    }
+
+}
---
+++ Assets/gameMusic.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gameMusic : MonoBehaviour {
+
+    // Use this for initialization
+
+    public AudioSource[] music;
+    public AudioSource music1;
+    public AudioSource music2;
+
+    public bool firstPlaying;
+
+    void Start() {
+        music = GetComponents<AudioSource>();
+        music1 = music[0];
+        music2 = music[1];
+
+        firstPlaying = false;
+
+        music1.loop = false;
+        music2.loop = false;
+
+        playMusic();
+    }
+
+    void playMusic() {
+
+        if (firstPlaying)
+        {
+            music1.Play();
+
+            firstPlaying = false;
+
+            Invoke("playMusic", 141f);
+
+        }
+
+        else
+        {
+            music2.Play();
+
+            firstPlaying = true;
+
+            Invoke("playMusic", 89f);
+
+        }
+    }
+
+
+}
