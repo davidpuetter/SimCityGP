512be6ab5881aee1e706aa4ac31292869cf57a02
AKnowles96 <ashknowles1996@gmail.com>
2017-04-26 19:44:17 +0100

    Final Merge (w/ menu)
    Hopefully should work.

---
+++ Assets/MenuScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MenuScript : MonoBehaviour {
+
+	void Start ()
+    {
+		
+	}
+
+	void Update ()
+    {
+		
+	}
+
+    public void LoadLevel()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
